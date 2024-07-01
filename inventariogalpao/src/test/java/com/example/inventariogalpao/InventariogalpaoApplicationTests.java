4952eb241b3b78c1747ffd59c2a753315c9731b4
henriquetg1 <henriqueturco1@gmail.com>
2024-07-01 10:54:55 -0300

    build: Inicialização do backend

---
+++ inventariogalpao/src/test/java/com/example/inventariogalpao/InventariogalpaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventariogalpao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariogalpaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
